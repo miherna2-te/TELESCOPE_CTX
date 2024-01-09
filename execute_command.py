cf525aeb13a13ea8aedb9a44e5c2bea27cc4c108
Miguel Hernandez <miherna2@thousandeyes.com>
2024-01-08 18:09:18 -0600

    accounts, endpoints and tests working as expected. Debug command is working too

--- execute_command.py
+++ execute_command.py
@@ -3,7 +3,6 @@ import requests
 import json
 from jinja2 import Environment, FileSystemLoader
 from rich.console import Console
-from rich import print
 import os
 from datetime import datetime
 
@@ -12,90 +11,50 @@ class DataProcessor:
     def __init__(self):
         self.console = Console()
         os.makedirs("./output", exist_ok=True)
+        self.env = Environment(loader=FileSystemLoader("./templates"))
 
     def api_get_data(self, username, password, call, filter):
-        headers = {"content-type": "application/json"}
         url = f"https://api.thousandeyes.com/v6/{call}.json"
         if filter:
             url += f"?aid={filter}"
         try:
             response = requests.get(
                 url=url,
-                headers=headers,
+                headers={"content-type": "application/json"},
                 auth=(username, password),
             )
-            response.raise_for_status()  # Esto generará una excepción si el estado HTTP indica un error
+            response.raise_for_status()
         except requests.HTTPError as http_err:
-            status_code = http_err.response.status_code
-            if status_code == 401:
-                return "Error: Unauthorized. Please check your username and password."
-            elif status_code == 403:
-                return "Error: Forbidden. You do not have the necessary permissions."
-            elif status_code == 404:
-                return "Error: Not Found. The requested resource could not be found."
-            else:
-                return f"An HTTP Error occurred: {http_err}"
+            return f"An HTTP Error occurred: {http_err}"
         except Exception as err:
             return f"An Error occurred: {err}"
-
         return response.json()
 
-    def to_yaml(self, data, resource, write):
+    def to_file(self, data, resource, format, write):
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        output = yaml.dump(data)
-        if write:
-            with open(f"./output/{resource}_{timestamp}.yaml", "w") as f:
-                f.write(output)
+        output = ""
+        if format == "json":
+            output = json.dumps(data, indent=4)
+        elif format in ["yaml", "csv", "human"]:
+            if format == "yaml":
+                output = yaml.dump(data)
+            else:
+                template = self.env.get_template(f"{resource}_{format}.j2")
+                output = template.render(data=data)
         else:
-            self.console.print(output)
+            self.console.print("Format is invalid", style="bold red")
+            return
 
-    def to_human(self, data, resource, write):
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        file_loader = FileSystemLoader("./templates_human")
-        env = Environment(loader=file_loader)
-        template = env.get_template(f"{resource}.j2")
-        output = template.render(data=data)
         if write:
-            with open(f"./output/{resource}_{timestamp}.txt", "w") as f:
-                f.write(output)
-        else:
-            self.console.print(output)
-
-    def to_json(self, data, resource, write):
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        output = json.dumps(data, indent=4)
-        if write:
-            with open(f"./output/{resource}_{timestamp}.json", "w") as f:
-                f.write(output)
-        else:
-            self.console.print(output)
-
-    def to_csv(self, data, resource, write):
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        file_loader = FileSystemLoader("./templates_csv")
-        env = Environment(loader=file_loader)
-        template = env.get_template(f"{resource}.j2")
-        output = template.render(data=data)
-        if write:
-            with open(f"./output/{resource}_{timestamp}.csv", "w") as f:
+            with open(f"./output/{resource}_{timestamp}.{format}", "w") as f:
                 f.write(output)
         else:
             self.console.print(output)
 
     def __call__(self, username, password, call, format, filter, write, resource):
         data = self.api_get_data(username, password, call, filter)
-
         if "Error" in data:
             self.console.print(data, style="bold red")
-        elif format == "yaml":
-            self.to_yaml(data, resource, write)
-        elif format == "human":
-            self.to_human(data, resource, write)
-        elif format == "json" or format is None:
-            self.to_json(data, resource, write)
-        elif format == "csv":
-            self.to_csv(data, resource, write)
         else:
-            self.console.print("Format is invalid", style="bold red")
-
+            self.to_file(data, resource, format or "json", write)
         return ""
--- main.py
+++ main.py
@@ -1,123 +1,98 @@
 import re
 import sys
-from getpass import getpass
+import socket
 import readline
+from getpass import getpass
 from execute_command import DataProcessor
 from rich import print
 from rich.console import Console
-import socket
-from urllib.request import urlopen, URLError
 from rich.panel import Panel
 
 console = Console()
 
 
-def is_valid_email(email):
-    return bool(re.match(r"^[\w\.-]+@[\w\.-]+\.\w+$", email))
-
-
-def is_valid_token(token):
-    return len(token) == 32
-
-
-def check_api_access():
-    try:
-        socket.gethostbyname("api.thousandeyes.com")
-        return True
-    except socket.gaierror:
-        return False
-
-
 def parse_command(command_str):
     resource = re.search(r"show\s+(.*?)(?=\s+write|\s+filter|\s+format|$)", command_str)
     format = re.search(r"format\s+(yaml|csv|json|human)", command_str)
-    write = "write" in command_str
-    filter = re.search(r"filter\s+(\d+)", command_str)
-
-    if "format" in command_str and not format:
-        raise ValueError(
-            "Command incorrect. After 'format' must follow 'yaml', 'csv', 'json', or 'human'."
-        )
-    if "filter" in command_str and not filter:
-        raise ValueError(
-            "Command incorrect. After 'filter' must follow a string of numbers."
-        )
-
-    resource = resource.group(1).strip() if resource else None
-    format = format.group(1) if format else "json"
-    filter = filter.group(1) if filter else None
-
-    return resource, format, write, filter
+    return (
+        resource and resource.group(1).strip(),
+        format and format.group(1) or "json",
+        "write" in command_str,
+        re.search(r"filter\s+(\d+)", command_str),
+    )
 
 
 def main():
-    welcome_banner = "ThousandEyes: Cortex Welcome"
-    console.print(Panel(welcome_banner, border_style="white"), style="bold white")
+    console.print(
+        Panel("ThousandEyes/Telescope Welcome", border_style="orange_red1"),
+        style="orange_red1",
+    )
+
     username = input("Email: ")
-    if not is_valid_email(username):
-        console.print("Invalid email, please try again.", style="bold red")
+    if not re.match(r"^[\w\.-]+@[\w\.-]+\.\w+$", username):
+        console.print("Error: Invalid email, please try again.", style="bold red")
         sys.exit()
 
     password = getpass("Basic Authentication Token: ")
-    if not is_valid_token(password):
-        console.print("Invalid token, please try again.", style="bold red")
+    if len(password) != 32:
+        console.print("Error: Invalid token, please try again.", style="bold red")
         sys.exit()
 
-    api_status = "Accessible" if check_api_access() else "Not accessible"
-    console.print(f"Welcome, {username}!", style="bold green")
-    console.print(f"ThousandEyes API status: {api_status}", style="bold green")
+    api_status = (
+        "Accessible"
+        if socket.gethostbyname("api.thousandeyes.com")
+        else "Not accessible"
+    )
+    console.print(f"Welcome, {username}!\nThousandEyes API status: {api_status}")
     resources = {
         "tests": "tests.json",
         "accounts": "account-groups.json",
         "tests details": "tests_details.json",
         "endpoints": "endpoint-agents.json",
     }
-    commands = resources.keys()
-    debug_enabled = False
     processor = DataProcessor()
+    debug_enabled = False
     while True:
         try:
             command_str = input("cortex# ")
             readline.add_history(command_str)
-
-            if not command_str:
+            if not command_str or command_str.lower() == "ls":
+                console.print(
+                    "\n".join([f"show {cmd}" for cmd in resources.keys()]),
+                    style="bold green",
+                )
                 continue
-
-            if command_str.lower() == "ls":
-                for cmd in commands:
-                    console.print(f"show {cmd}", style="bold green")
-                    continue
-
-            if command_str.lower().startswith("show"):
+            if command_str.lower() == "show run":
+                console.print(f" Username: {username}")
+                console.print(f" Password: {password}")
+                console.print(f" Debug: {debug_enabled}")
+                console.print(f" API Status: {api_status}")
+            elif command_str.lower().startswith("show"):
                 resource, format, write, filter = parse_command(command_str)
-
                 if resource not in resources:
-                    commands_str = ", ".join(commands)
                     console.print(
-                        f"Invalid resource. Please use: {commands_str}",
+                        f"Invalid resource. Please use: {', '.join(resources.keys())}",
                         style="bold red",
                     )
                     continue
-
                 call = resources[resource]
                 output = processor(
                     username, password, call, format, filter, write, resource
                 )
-                # console.print(output)
-
+                if debug_enabled:
+                    console.print(
+                        f"{resource=} {call=} {format=}, {write=} {call=} {filter=}"
+                    )
             elif command_str.lower() == "exit":
                 break
-
-            if command_str.lower() == "debug enabled":
-                debug_enabled = True
-
-            if command_str.lower() == "debug disabled":
-                debug_enabled = False
-
-            if debug_enabled:
+            elif command_str.lower() in ["debug enabled", "debug disabled"]:
+                debug_enabled = command_str.lower() == "debug enabled"
+            else:
                 console.print(
-                    f"{resource=} {call=} {format=}, {write=} {call=} {filter=}"
-                )
+                            f"Invalid resource. Please use: {', '.join(resources.keys())}",
+                            style="bold red",
+                        )
+                continue
         except Exception as e:
             console.print(f"An error occurred: {e}", style="bold red")
 
