9b595936a41b7149fd405bae2f50fd939299e950
Miguel Hernandez <miherna2@thousandeyes.com>
2024-01-18 14:17:24 -0600

    adding endpoint tests scheduled tests

---
+++ execute/show_endpoint_tests.py
@@ -0,0 +1,7 @@
+from .execute_commands import ShowCommand
+
+
+def show_endpoint_tests(token, format, filter, write):
+    endpoint_tests = ShowCommand(token)
+    show_endpoint_tests = endpoint_tests("endpoint/tests/scheduled-tests", format, filter, write)
+    return show_endpoint_tests
